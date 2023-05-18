2984b854575c5e0ba35c4057801db2c0d315201d
Klimov_Maxim <120740542+MakSimKlim@users.noreply.github.com>
2023-05-18 19:52:27 +0500

    Add files via upload

--- 230516_3 функции min max.cpp
+++ 230516_3 функции min max.cpp
@@ -25,14 +25,35 @@ void FillArray(int ar[], int size) // функция заполнения мас
 int Max(int ar[], int size)
 {
 	int max = 0;
-	int index;
-	for (int i = 0; i < size; i++)
+	int index;//особенность для максимума
+	for (int i = 1; i < size; i++)//начали с 1 т.к. 0 элемент уже приняли за минимум
 	{
-		if
-		index = i;
-		max = ar[i];
+		if (ar[i] > max)//это условие всегда выполнится хотя бы один раз, т.к. хотя бы один элемент будет максимальным, поэтому index ни к чему изначально не приравнивается
+		{
+			index = i;
+			max = ar[i];
+		}
+		
 	}
+	cout << "\nFound max=" << max << " at " << index + 1 << "\n";
+	return index;
+}
 
+int Min(int ar[], int size)
+{
+	int min = ar[0];
+	int index = 0;//особенность для минимума
+	for (int i = 1; i < size; i++)//начали с 1 т.к. 0 элемент уже приняли за минимум
+	{
+		if (ar[i]<min)//это условие никогда не выполнится если 0 элемент = 0, поэтому index =0;
+		{
+			index = i;//новый индекс минимального элемента
+			min = ar[i];//новый элемент, равный минимуму
+		}
+
+	}
+	cout << "\nFound min=" << min << " at " << index + 1 << "\n";
+	return index;
 }
 
 
@@ -50,10 +71,26 @@ int main()
 	const int size = 10;
 	int ar[size];
 	FillArray(ar, size);
+	//вывод массива на экран
+	for (int i = 0; i < size; i++)
+	{
+		cout << ar[i] << " ";
+	}
+	cout << "\n";
+	Max(ar, size);
+	Min(ar, size);
+	cout << "\n";
 
-
-
-
+	int b[size];
+	FillArray(b, size);
+	//вывод массива на экран
+	for (int i = 0; i < size; i++)
+	{
+		cout << b[i] << " ";
+	}
+	cout << "\n";
+	Max(b, size);
+	Min(b, size);
 
 }
 
