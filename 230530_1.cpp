dab8fdddaf328bf3115f2e7346473c5662df89f7
Klimov_Maxim <120740542+MakSimKlim@users.noreply.github.com>
2023-05-30 20:40:33 +0500

    Add files via upload

---
+++ 230530_1.cpp
@@ -0,0 +1,25 @@
+﻿// 230530_1
+//
+// дано натуральное число N (целое, и оно 0 и больше). Вычислить сумму его цифр
+
+#include <iostream>
+
+using namespace std;
+
+unsigned int SumNatural(unsigned int a)
+{
+	if (a < 10)
+		return a;
+	else
+		return a % 10 + SumNatural(a / 10);
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	int a;
+	cout << "Введите число:" << "\n";
+	cin >> a;
+	cout << "Сумма цифр натурального числа:" << SumNatural(a);
+}
+
