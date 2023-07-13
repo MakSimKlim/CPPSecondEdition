290f16f584e27cca1b3ee630509395901b719311
Klimov_Maxim <120740542+MakSimKlim@users.noreply.github.com>
2023-07-13 21:17:41 +0500

    Add files via upload

---
+++ 230713_ООП_дилегирование конструктора/Point.cpp
@@ -0,0 +1,4 @@
+#include "Point.h"
+
+int Point::maxX{ 150 };
+int Point::maxY{ 150 };
---
+++ 230713_ООП_дилегирование конструктора/Point.h
@@ -0,0 +1,16 @@
+#pragma once
+
+class Point
+{
+public:
+	int x;
+	int y;
+	//const int maxX{ 1500 };
+	//const int maxy{ 1500 };
+
+	static int maxX; // статические переменные
+	static int maxY; // статические переменные
+
+};
+
+
