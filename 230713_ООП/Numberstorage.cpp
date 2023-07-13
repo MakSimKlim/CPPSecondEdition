4fc2711bfea7441ef14afcb8af8969ad17c44a55
Klimov_Maxim <120740542+MakSimKlim@users.noreply.github.com>
2023-07-13 21:37:00 +0500

    Numberstorage.cpp

--- 230713_ООП/Numberstorage.cpp
+++ 230713_ООП/Numberstorage.cpp
@@ -1,36 +1,46 @@
-#include "Numberstorage.h"
+#include "NumberStorage.h"
 
-Numberstorage::Numberstorage(uint32_t elementCountP)
-
-storage {new int[elementCountP] };
-
-elementCount{ elementCountP }
-	{
-		uint32_t used{ elemebtCount * sizeof(int) };
-		usedMemory += used;
-		std::cout << used << "bytes of additional memory used. Total: " << std::endl;
-		showTotalMemory();
-
-	}
-
-// ...
-
+NumberStorage::NumberStorage(uint32_t elementCountP) :
+	storage{ new int[elementCountP] },
+	elementCount{ elementCountP }
 {
-	uint32_t used{ elemebtCount * sizeof(int) };
-	usedMemory -= freed;
-	std::cout << "Freed" << freed<< "bytes. " << "Total used: " << std::endl;
+	uint32_t used{ elementCount * sizeof(int) };
+	usedMemory += used;
+	std::cout << used << " bytes of additional memory used. Total: ";
 	showTotalMemory();
 
+	for (int i = 0; i < elementCount; i++)
+	{
+		storage[i] = rand() % 100;
+	}
 }
 
-uint32_t Numberstorage::getTotalMemory()
+void NumberStorage::show()
+{
+	for (int i = 0; i < elementCount; i++)
+	{
+		std::cout << storage[i] << " ";
+	}
+	std::cout << "\n";
+}
+
+NumberStorage::~NumberStorage()
+{
+	uint32_t freed{elementCount * sizeof(int)};
+	delete[] storage;
+	usedMemory -= freed;
+	std::cout << "Freed " << freed << " bytes." << " Total used: ";
+	showTotalMemory();
+}
+
+uint32_t NumberStorage::getTotalMemory()
 {
 	return usedMemory;
 }
 
-void Numberstorage::showTotalMemory()
+void NumberStorage::showTotalMemory()
 {
-	std::cout << usedMemory << " bytes" << std::endl;
+	std::cout << usedMemory << " bytes"<< std::endl;
 }
 
-uint32_t Numberstorage::usedMemory{ 0 };
+uint32_t NumberStorage::usedMemory{ 0 };
