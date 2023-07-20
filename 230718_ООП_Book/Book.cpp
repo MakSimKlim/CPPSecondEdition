391a5127bb975ae5ffbd849f9a4e06d7bf99edb6
Klimov_Maxim <120740542+MakSimKlim@users.noreply.github.com>
2023-07-20 18:23:58 +0500

    Add files via upload

--- 230718_ООП_Book/Book.cpp
+++ 230718_ООП_Book/Book.cpp
@@ -1,6 +1,32 @@
 #include "Book.h"
 
-void Show()
+int Book::bookCount{ 0 };
+
+
+Book::Book(const char* nameA, const char* titleB, int yearP, int pageN) :
+	nameAuthor{ new char[strlen(nameA) + 1] },
+	titleBook{ new char[strlen(titleB) + 1] },
+	yearPrint{ yearP },
+	pageNumber{ pageN }
 {
-	std::cout << Book.nameAuthor << std::endl;
-}
+	memcpy_s(nameAuthor, strlen(nameA) + 1, nameA, strlen(nameA) + 1);
+	memcpy_s(titleBook, strlen(titleB) + 1, titleB, strlen(titleB) + 1);
+	bookCount++;
+};
+
+
+Book::Book(const char* nameA, const char* titleB):
+	Book{ nameA, titleB, 0, 0 }
+{};
+
+Book::Book(const char* nameA):
+	Book{ nameA , "" ,0, 0}
+{};
+Book::Book() :
+	Book{"","",0,0}
+{};
+
+Book::~Book()
+{
+	bookCount--;
+};
--- 230718_ООП_Book/Book.h
+++ 230718_ООП_Book/Book.h
@@ -3,22 +3,67 @@
 
 class Book
 {
-    char* nameAuthor[50];
-    char* titleBook[100];
+    char* nameAuthor;
+    char* titleBook;
     int yearPrint;
     int pageNumber;
+    static int bookCount;
 
 public:
-    Book(const char* nameA, const char* titleB, int yearP, int pageN);
 
-    Book(const char* nameA, const char* titleB );
+    // конструкторы
+    Book(const char* nameA, const char* titleB, int yearP, int pageN); // имя автора, название, год, кол-во страниц
+    Book(const char* nameA, const char* titleB);                       // имя автораб название
+    Book(const char* nameA);                                           // название
+    Book();                                                            // конструктор по-умолчанию
 
-    Book(const char* titleB);
+    // сеттеры, геттеры имя автора
+    char* GetNameAuthor()
+    {
+        return nameAuthor;
+    };
+    void SetNameAuthor(const char* nameA)
+    {
+        memcpy_s(nameAuthor, strlen(nameA) + 1, nameA, strlen(nameA) + 1);
+    };
+    // сеттеры, геттеры название книги
+    char* GetTitleBook()
+    {
+        return titleBook;
+    };
+    void SetTitleBook(const char* titleB)
+    {
+        memcpy_s(titleBook, strlen(titleB) + 1, titleB, strlen(titleB) + 1);
+    };
+    // сеттеры, геттеры год издания
+    int GetYearPrint()
+    {
+        return yearPrint;
+    };
+    void SetYearPrint(int yearP)
+    {
+       yearPrint = yearP;
+    };
+    // сеттеры, геттеры кол-во страниц
+    int GetPageNumber()
+    {
+        return pageNumber;
+    };
+    void SetPageNumber(int pageN)
+    {
+        pageNumber = pageN;
+    };
 
-    ~Book();
+    static int GetCount()
+    {
+        return bookCount;
+    }
 
-    void Show();
+    void print()
+    {
+        cout << "Book info : " << nameAuthor << titleBook << yearPrint << pageNumber;
+    }
 
-
+    ~Book();                                                        // деструктор
 
 };
