5766b83325bedc4e9e79f28463814f1941be93df
Klimov_Maxim <120740542+MakSimKlim@users.noreply.github.com>
2023-08-17 16:07:13 +0500

    Add files via upload

--- Airplane_class.cpp
+++ Airplane_class.cpp
@@ -1,9 +1,9 @@
 ﻿// Airplane_class
 // 
-// Создайте класс Airplane, он содержит номер рейса и количество пассажиров.
-// Перегрузите операторы сравнения (сравнивайте по количеству пассажиров)
-// создайте ему конструкторы копирования и перемещения,
-// а также перегрузку для оператора копир. и перем. присваивания
+ //Создайте класс Airplane, он содержит номер рейса и количество пассажиров.
+ //Перегрузите операторы сравнения (сравнивайте по количеству пассажиров)
+ //создайте ему конструкторы копирования и перемещения,
+ //а также перегрузку для оператора копир. и перем. присваивания
 //
 //  
 // https://codepal.ai/code-generator/query/69iYuoEo/function-cpp-airplane-class
@@ -16,34 +16,29 @@ using namespace std;
 
 class Airplane
 {
+private:
+
+	string NumberFlights;
+	int NumberPassengers;
+
 public:
-	// обычный конструктор 
+	// конструктор по умолчанию
+	Airplane()
+	{
+		NumberFlights = "";
+		NumberPassengers = 0;
+		cout << "Применён конструктор по умолчанию" << endl;
+
+	}
+
+	// конструктор с параметрами
 	Airplane(string numFlights, int numPassengers)
 	{
 		NumberFlights = numFlights;
 		NumberPassengers = numPassengers;
-	}
+		cout << "Применён конструктор с параметрами" << endl;
 
-	// Перегрузка оператора сравнения (сравнивает по количеству пассажиров на рейсах)
-	bool operator>(const Airplane& f)
-	{
-		return NumberPassengers > f.NumberPassengers;
 	}
-
-	// перегрузка оператора копирования
-	bool operator=(const Airplane& a)
-	{
-		cout << "перегрузка оператора копирования";
-		return NumberPassengers = a.NumberPassengers;
-	}
-	// перегрузка оператора перемещения
-	bool operator=(Airplane&& m)
-	{
-		cout << "перегрузка оператора перемещения";
-		return NumberPassengers = m.NumberPassengers;
-	}
-
-
 
 	// конструктор копирования
 	// По умолчанию компилятор при компиляции классов генерирует специальный конструктор - 
@@ -54,7 +49,7 @@ public:
 	{
 		NumberFlights = copy.NumberFlights;
 		NumberPassengers = copy.NumberPassengers + 10; // +10 для примера увеличивает кол-во пассажиров на рейсе на 10
-		cout << "Применён конструктор копирования" << endl;
+		cout << "Применён конструктор копирования с модификацией параметров" << endl;
 
 	}
 
@@ -64,20 +59,62 @@ public:
 	// вместо копирования данных они просто перемещаются из одной копии объекта в другую
 	// Чтобы указатель перемещаемого объекта moved перестал указывать на эту область памяти, 
 	// и соответственно чтобы в деструкторе объекта moved не было ее освобождения, передаем указателю значение nullptr.
-	// В приведенной выше общей форме используется ключевое слово noexcept. Этот спецификатор указывает, 
-	// что наша функция (конструктор переноса) не генерирует (не выбрасывает) исключение или аварийно завершает свою работу.
-	Airplane(Airplane&& moved) noexcept
+	Airplane(Airplane&& moved)
 	{
-		NumberFlights = moved.NumberFlights;      // перемещаем кол-во пассажиров
-		NumberPassengers = moved.NumberPassengers;// перемещаем номер рейса
+		NumberFlights = move(moved.NumberFlights);      // перемещаем кол-во пассажиров
+		NumberPassengers = move(moved.NumberPassengers);// перемещаем номер рейса
+		moved.NumberFlights.clear();                    // метод для очистки строк string
+		moved.NumberPassengers = 0;
 		cout << "Применён конструктор перемещения" << endl;
-		
-		//moved.NumberFlights = nullptr;
-		//moved.NumberPassengers = 0;
+
 	}
+
+	// Перегрузка операторов сравнения (сравнивает по количеству пассажиров на рейсах)
+	// (больше)
+	bool operator>(const Airplane& f)
+	{
+		return NumberPassengers > f.NumberPassengers;
+	}
+	// (меньше)
+	bool operator<(const Airplane& f)
+	{
+		return NumberPassengers < f.NumberPassengers;
+	}
+	// (равно)
+	bool operator==(const Airplane& f)
+	{
+		return NumberPassengers == f.NumberPassengers;
+	}
+
+
+	// перегрузка Оператора копирования присваивания
+	Airplane& operator=(const Airplane& f) 
+	{
+		if (this != &f) 
+		{
+			NumberFlights = f.NumberFlights;
+			NumberPassengers = f.NumberPassengers;
+		}
+		return *this;
+	}
+
+	// перегрузка Оператора перемещения присваивания
+	Airplane& operator=(Airplane&& moved) noexcept 
+	{
+		if (this != &moved) 
+		{
+			NumberFlights = move(moved.NumberFlights);
+			NumberPassengers = move(moved.NumberPassengers);
+			moved.NumberPassengers = 0;
+		}
+		return *this;
+	}
+
+	// Деструктор
+	~Airplane()
+	{
 		
-		
-	
+	}
 
 	// метод вывода информации о рейсах в консоль
 	void print()
@@ -85,14 +122,6 @@ public:
 		cout << "Номер рейса: " << NumberFlights << "\tКол-во пассажиров: " << NumberPassengers << endl;
 	}
 
-	//// Деструктор
-	~Airplane()
-	{	};
-
-private:
-
-	string NumberFlights;
-	int NumberPassengers;
 
 };
 
@@ -101,7 +130,7 @@ int main()
 {
 	setlocale(LC_ALL, "Rus");
 
-	// Создание двух рейсов (f) - Название рейса - Кол-во пассажиров
+	// Создание двух рейсов через конструктор с параметрами (f) - Название рейса - Кол-во пассажиров
 	Airplane f1("Su 1093", 125);// рейс Челябинск - Москва
 	Airplane f2("Dp 550", 130); // рейс Челябинск - Санкт-Петербург
 	// вывод информации о рейсах:
@@ -113,9 +142,13 @@ int main()
 	{
 		cout << "Рейс Su 1093 может взять на борт больше пассажиров" << endl;
 	}
-	else
+	else if (f1 < f2)
 	{
 		cout << "Рейс Dp 550 может взять на борт больше пассажиров" << endl;
+	}
+	else
+	{
+		cout << "Рейс Dp 550 может взять на борт столько же пассажиров сколько рейс Su 1093" << endl;
 	}
 
 	cout << endl;
@@ -128,30 +161,18 @@ int main()
 
 
 	cout << endl;
-	//// Создание объектов f5 f6 на основе объектов f1 f2 через конструктор перемещения
+	// Создание объектов f5 f6 на основе объектов f1 f2 через конструктор перемещения
 	Airplane f5 = move(f1);
 	Airplane f6 = move(f2);
 	// вывод информации о рейсах:
 	f5.print();
 	f6.print();
 
-	// проверка о срабатывании оператора перемещения, исходник =0
 	cout << endl;
-	cout << "проверка о срабатывании оператора перемещения, исходник =0" << endl;
-	cout << "size f1 = " << sizeof f1 << "; size f5= " << sizeof f5 << endl;
-	cout << "size f2 = " << sizeof f2 << "; size f6= " << sizeof f6 << endl << endl;
-
-	// перегрузка оператора копирования
-	f3 = f1;
-	cout << endl;
-	f4 = f2;
-	cout << endl << endl;
-
-	// перегрузка оператора перемещения
-	f5 = move(f1);
-	cout << endl;
-	f6 = move(f2);
-	cout << endl << endl;
+	cout << "Проверка, что перемещение удалось f1, f2 после перемещения:" << endl;
+	f1.print();
+	f2.print();
 
 	return 0;
 }
+
